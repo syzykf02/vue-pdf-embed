4703e12038fe49200906bedbaa7c1b5c3ebf0e7a
syzykf02 <syzykf02@126.com>
2025-04-20 12:28:35 +0800

    init

---
+++ dev/App.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import VuePdfEmbed from '../src/index'
+
+const pdfSource = '大语言模型的幻觉问题研究综述.pdf'
+</script>
+
+<template>
+  <VuePdfEmbed annotation-layer text-layer :source="pdfSource" />
+</template>
+
+<style lang="scss">
+@import 'pdfjs-dist/web/pdf_viewer.css';
+
+body {
+  padding: 16px;
+  background-color: #a8a7a7;
+}
+
+.vue-pdf-embed {
+  margin: auto;
+  max-width: 1024px;
+
+  & > div {
+    margin-bottom: 4px;
+    box-shadow: 0 2px 8px 4px rgba(0, 0, 0, 0.1);
+  }
+}
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,55 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import copy from 'rollup-plugin-copy'
+import CleanCSS from 'clean-css'
+import type { RollupOptions } from 'rollup'
+
+export const rollupOptions: RollupOptions = {
+  external: ['pdfjs-dist', 'vue'],
+  output: {
+    globals: {
+      'pdfjs-dist': 'pdfjsLib',
+      vue: 'Vue',
+    },
+    compact: true,
+    inlineDynamicImports: true,
+  },
+}
+
+export default defineConfig({
+  plugins: [
+    copy({
+      hook: 'writeBundle',
+      targets: Object.entries({
+        textLayer: [
+          [3103, 3112],
+          [582, 709],
+        ],
+        annotationLayer: [
+          [3103, 3112],
+          [710, 1074],
+        ],
+      }).map(([key, ranges]) => ({
+        src: 'node_modules/pdfjs-dist/web/pdf_viewer.css',
+        dest: 'dist/styles',
+        rename: `${key}.css`,
+        transform: (contents) => {
+          const lines = contents.toString().split('\n')
+          const css = ranges.reduce((acc, [start, end]) => {
+            return acc + lines.slice(start, end).join('\n')
+          }, '')
+          return new CleanCSS().minify(css).styles + '\n'
+        },
+      })),
+    }),
+    vue(),
+  ],
+  build: {
+    lib: {
+      entry: new URL('./src/index.ts', import.meta.url).pathname,
+      name: 'VuePdfEmbed',
+      fileName: 'index',
+    },
+    rollupOptions,
+  },
+})
